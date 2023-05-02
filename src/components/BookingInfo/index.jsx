9092209b8e4b17ae8b8a1a5c083a17350c96aa8c
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2023-05-02 17:42:24 +0200

    add BookingInfo

---
+++ src/components/BookingInfo/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import useProfileData from "../ProfileData";
+
+
+function BookingInfo() {
+  const profileData = useProfileData();
+
+  const venues = profileData ? profileData.venues : 0;
+  const bookingsArray = profileData ? profileData.bookings : 0;
+  
+  return (
+    <div>BookingInfo</div>
+  )
+}
+
+export default BookingInfo
