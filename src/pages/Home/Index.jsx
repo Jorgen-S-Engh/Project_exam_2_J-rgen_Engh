4e515813085854c8374471535058831daf34180b
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2023-05-16 15:03:09 +0200

    work on venue card

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Venues from '../../components/Venues'
+import './Home.module.scss'
 
 function Home() {
   return (
