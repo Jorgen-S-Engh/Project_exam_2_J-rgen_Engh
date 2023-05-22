f0d04ddea2983d42abf6d46afe5c1518c4928068
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2023-05-22 12:25:00 +0200

    Work on calendar

--- src/pages/SingleVenue/index.jsx
+++ src/pages/SingleVenue/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Header from '../../components/Header/Header';
@@ -6,12 +6,15 @@ import styles from "./SingleVenue.module.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-// import MyCalendar from '../../components/Calendar';
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-
-
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { formatISO } from 'date-fns';
+import breakfast from "../../assets/breakfast.png"
+import pets from "../../assets/pawprint.png"
+import wifi from "../../assets/wifi.png"
+import parking from "../../assets/parked-car.png"
+import guests from "../../assets/group.png"
 
 function NextArrow(props) {
   const { className, style, onClick } = props;
@@ -40,6 +43,8 @@ function SingleVenue() {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [events, setEvents] = useState([]);
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedEndDate, setSelectedEndDate] = useState(null);
   let { id } = useParams();
 
   useEffect(() => {
@@ -50,7 +55,7 @@ function SingleVenue() {
 
         const response = await fetch(url);
         const json = await response.json();
-        console.log(json)
+        console.log(json);
 
         setData(json);
         setEvents(json.bookings.map(booking => ({
@@ -59,8 +64,6 @@ function SingleVenue() {
           end: new Date(booking.dateTo).toISOString(),
           allDay: true
         })));
-
-        
       } catch (error) {
         setIsError(true);
         console.log(error);
@@ -72,8 +75,69 @@ function SingleVenue() {
     getData(`https://api.noroff.dev/api/v1/holidaze/venues/${id}?_bookings=true`);
   }, [id]);
 
+  const checkAvailability = (start, end) => {
+    const startDateISO = formatISO(start, { representation: 'date' });
+    const endDateISO = formatISO(end, { representation: 'date' });
+  
+    const isBooked = events.some(event => {
+      const eventStart = formatISO(new Date(event.start), { representation: 'date' });
+      const eventEnd = formatISO(new Date(event.end), { representation: 'date' });
+  
+      return (startDateISO >= eventStart && startDateISO <= eventEnd) || 
+             (endDateISO >= eventStart && endDateISO <= eventEnd) ||
+             (startDateISO <= eventStart && endDateISO >= eventEnd);
+    });
+  
+    return !isBooked;
+  };
+  
+  const handleBooking = async () => {
+    if (!selectedStartDate || !selectedEndDate) {
+      alert('Please select a start and end date');
+      return;
+    }
+  
+    if (!checkAvailability(new Date(selectedStartDate), new Date(selectedEndDate))) {
+      alert('The selected date range is already booked');
+      return;
+    }
+  
+    try {
+      const accessToken = localStorage.getItem('accessToken');
+      const response = await fetch('https://api.noroff.dev/api/v1/holidaze/bookings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+          venueId: id,
+          dateFrom: selectedStartDate,
+          dateTo: selectedEndDate,
+          guests: 1,
+        }),
+      });
+      console.log(response)
+  
+      const newBooking = await response.json();
+      const endDate = new Date(selectedEndDate);
+      endDate.setDate(endDate.getDate() + 1); // Make end date inclusive
+      if (newBooking) {
+        setEvents([...events, {
+          title: 'Booked',
+          start: new Date(selectedStartDate).toISOString(),
+          end: endDate.toISOString(),  // Updated line
+          allDay: true
+        }]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
   if (isLoading || !data) {
-    return <div>Loading</div>;
+    return <div>Loading...</div>;
   }
 
   if (isError) {
@@ -91,7 +155,7 @@ function SingleVenue() {
   };
 
   return (
-    <div>
+    <div className={styles.component_container}>
       <Header/>
       <div className={styles.image_container}>
         {data.media && (
@@ -104,18 +168,43 @@ function SingleVenue() {
           </Slider>
         )}
       </div>
-      <h1>{data.name}</h1>
+      <div className={styles.heading_container}>
+        <h1>{data.name}</h1>
+        <h2>Description</h2>
+        <p>{data.description}</p>
+      </div>
+      <div className={styles.meta_container}>
+          {data.meta.wifi && <img src={wifi} alt="wifi icon"/>}
+          {data.meta.pets && <img src={pets} alt="pets icon"/>}
+          {data.meta.breakfast && <img src={breakfast} alt="breakfast icon"/>}
+          {data.meta.parking && <img src={parking} alt="parking icon"/>}
+      </div>
+      <div className={styles.info_container}>
+          <p>Price: {data.price}</p>
+          <p>Rating: {data.rating}</p>
+      </div>
+
       <div className={styles.calendar_container}>
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin]}
           initialView="dayGridMonth"
           events={events}
-        />
-     </div>
+          headerToolbar={{
+            left: 'prev,next',
+            center: '',
+            right: 'title'
+          }}/>
+      </div>
+      <div className={styles.booking_container}>
+        <h2>Book your holiday</h2>
+        <label htmlFor="date_from">From date</label>
+        <input type="date" id="date_from" value={selectedStartDate || ''} onChange={(e) => setSelectedStartDate(e.target.value)} />
+        <label htmlFor="date_to">To date</label>
+        <input type="date" id="date_to" value={selectedEndDate || ''} onChange={(e) => setSelectedEndDate(e.target.value)} />
+        <button className={styles.btn_submit_date} onClick={handleBooking}>Book Now</button>
     </div>
-
-  )
+    </div>
+  );
 }
 
-
-export default SingleVenue
+export default SingleVenue;
